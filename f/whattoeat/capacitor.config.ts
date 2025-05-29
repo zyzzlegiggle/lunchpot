46a9c073664deccc57f664c3750aa6263ab6902e
zyzzlegiggle <112403713+zyzzlegiggle@users.noreply.github.com>
2025-05-29 19:37:05 +0800

    icon and splash screen

--- f/whattoeat/capacitor.config.ts
+++ f/whattoeat/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'whattoeat',
+  appId: 'com.chupchip.app',
+  appName: 'chupchip',
   webDir: 'www'
 };
 
